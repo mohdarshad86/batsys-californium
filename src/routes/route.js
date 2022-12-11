43ac1f19edb16ae1ffc3494d8f56ccbc9d5f857c
Mohd Arshad <mohdarshad8668@gmail.com>
2022-12-11 15:53:24 +0530

    Assignment based on API

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,32 +1,92 @@
 const express = require("express");
 const router = express.Router();
-const intro = require("./introduction");
-const employee = require("./employee");
-const _ = require("underscore");
 
-router.get("/test-me", function (req, res) {
-  console.log("email from introduction module", intro.myEmail);
-  intro.myFunction("Sabiha");
-  console.log("email from employee module", employee.myEmail);
+//p1
+router.get("/movies", function (req, res) {
+  //route handeler
 
-  const days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
+  const movies = [
+    "Man of Steel",
+    "Batman v Superman",
+    "Wonder Women",
+    "Zack Snyder's Justice League",
   ];
-  let result = _.first(days, 4);
-  console.log(`Result from underscore function is ${result}`);
 
-  res.send("any dummy text");
+  res.json(movies);
 });
 
-router.get("/test-you", function (req, res) { //route handeler
-  console.log("I am here");
-  res.send("very important text");
+//p2
+router.get("/movies/:indexNumber", function (req, res) {
+  let i = req.params.indexNumber;
+
+  // route handler
+  const movies = [
+    "Man of Steel",
+    "Batman v Superman",
+    "Wonder Women",
+    "Zack Snyder's Justice League",
+  ];
+  // res.json(movies[i]);
+
+  //p3
+  // if (i > 0 && i < movies.length) {
+  //   res.json(movies[i]);
+  // } else {
+  //   res.json("Index is not defined, use a valid index");
+  // }
+});
+//p4
+router.get("/films", function (req, res) {
+  const film = [
+    {
+      id: 1,
+      name: "The Shining",
+    },
+    {
+      id: 2,
+      name: "Incendies",
+    },
+    {
+      id: 3,
+      name: "Rang de Basanti",
+    },
+    {
+      id: 4,
+      name: "Finding Nemo",
+    },
+  ];
+
+  res.json(film);
+});
+
+//p5
+router.get("/films/:filmId", function (req, res) {
+  let index = req.params.filmId;
+  const films = [
+    {
+      id: 1,
+      name: "The Shining",
+    },
+    {
+      id: 2,
+      name: "Incendies",
+    },
+    {
+      id: 3,
+      name: "Rang de Basanti",
+    },
+    {
+      id: 4,
+      name: "Finding Nemo",
+    },
+  ];
+
+  let film = films.find((movie) => movie.id == index);
+  if (film) {
+    res.json(film);
+  } else {
+    res.send("No movie exists with this id");
+  }
 });
 
 module.exports = router;
