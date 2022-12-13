090a6f9f8234175c7bb7f41d8096132b8f96ead3
Mohd Arshad <mohdarshad8668@gmail.com>
2022-12-13 20:19:41 +0530

    assignment on api

---
+++ practice.js
@@ -0,0 +1,50 @@
+(() => {
+  let x = (y = 10);
+})();
+
+console.log(typeof x);
+console.log(typeof y);
+const express = require('express');
+const router = express.Router();
+
+
+
+ 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+ 
+       //LOGIC WILL COME HERE
+       res.send(  { data: players , status: true }  )
+   })
+  
+module.exports = router;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,77 +1,57 @@
-const express = require('express');
+const e = require("express");
+const express = require("express");
 const router = express.Router();
-const intro = require('./introduction')
-const employee = require('./employee')
-const _ = require('underscore')
-const mentorModule = require('../abc/xyz/myModule'); 
-const req = require('express/lib/request');
-const { route } = require('express/lib/application');
+const { route } = require("express/lib/application");
 
+// ques- add Element in array
 
-router.get("/profile-details", function(req, res){
-    // Write the LOGIC here
-    res.send('dummy response')
-})
+router.post("/test-post4", function (req, res) {
+  let arr = [12, "functionup"];
+  let ele = req.body.element;
 
-router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
+  arr.push(ele);
 
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
-    console.log(`The mentor of the day is ${mentorModule.mentor}`)
-
-    res.send('any dummy text from route handler 1')
+  res.send({ msg: arr, status: true });
 });
 
+//assignment 1
 
-router.get('/test-me', function(req, res){
-    console.log("I am here")
-    res.send("any dummy text from route handler 2")
-})
+let players = [
+  {
+    name: "batman",
+    dob: "1/1/1989",
+    gender: "male",
+    city: "gotham",
+    sports: ["swimming"],
+  },
+  {
+    name: "superman",
+    dob: "1/09/1976",
+    gender: "male",
+    city: "metropolis",
+    sports: ["soccer"],
+  },
+  {
+    name: "cyborg",
+    dob: "1/1/1990",
+    gender: "male",
+    city: "central city",
+    sports: ["soccer"],
+  },
+];
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
+router.post("/players", function (req, res) {
+  let bodyName = req.body.name;
 
-// PATH Param example
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+  let play = players.find((x) => x.name === bodyName);
 
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use many ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
-
-// PATH Param example
-router.get("/profile/:name", function(req, res){
-    console.log('Printing the request to find out wjere name is stored',req.params)
-    console.log('user name is',req.params.name)
-    //console.log(`User requesting for profile is ${name}`)
-    res.send("dummy details")
-})
-
-// Query Param example
-router.get("/shoes", function(req, res){
-    console.log("The filter options for shoes are -",req.query)
-    //req.query.size
-    //req.query.brand
-    res.send("dummy shoes response")
-})
+  if (play) {
+    res.send({ data: players, status: true });
+  } else {
+    //just push the body object to the players object
+    players.push(req.body);
+    res.json({ data: players, status: true });
+  }
+});
 
 module.exports = router;
