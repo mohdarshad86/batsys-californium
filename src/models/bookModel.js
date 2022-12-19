cb1f1cfa2f336126ebb995dc49d2ce86045d49bd
Mohd Arshad <mohdarshad8668@gmail.com>
2022-12-19 17:08:54 +0530

    MongoDB Session 2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema( {
     bookName: String, 
     authorName: String, 
+    authorid:Number,
     tags: [String],
     
     isPublished: Boolean,
@@ -10,7 +11,12 @@ const bookSchema = new mongoose.Schema( {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    sales: {type: Number, default: 10},
+    summary : mongoose.Schema.Types.Mixed //"best book", ["nodeJs", "mongoDB", "frontEnd"]
+    /**
+     * "ch1": "intro to Js1"
+     * "ch2": "intro to Js2"
+     */
 }, { timestamps: true });
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -11,9 +11,9 @@ const bookControllerAs = require("../controllers/bookControllerAs");
 // router.post("/createUser", UserController.createUser);
 // router.get("/getUsersData", UserController.getUsersData);
 
-//Books
-// router.post("/createBook", BookController.createBook);
-// router.get("/getBooksData", BookController.getBooksData);
+//Session 1
+router.post("/createBook", BookController.createBook);
+router.get("/getBooksData", BookController.getBooksData);
 
 //Assignment
 router.post("/createBookAs", bookControllerAs.createBookAs);
@@ -22,5 +22,10 @@ router.post("/getBooksInYear", bookControllerAs.getBooksInYear);
 router.post("/getParticularBooks", bookControllerAs.getParticularBooks);
 router.get("/getXINRBooks", bookControllerAs.getXINRBooks);
 router.get("/getRandomBooks", bookControllerAs.getRandomBooks);
+
+//session 2
+router.post("/updataBooks", BookController.updataBooks);
+router.post("/getid", BookController.getid);
+
 
 module.exports = router;
