b21e78e9f1ae0fe839fe0d2706d79b4d43d061af
Mohd Arshad <mohdarshad8668@gmail.com>
2022-12-20 11:24:01 +0530

    Assignment w6d2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://mohdarshad86:Arshad86@cluster0.r4p7rwf.mongodb.net/Arshad86-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+
+const authorController = require("../controllers/controller");
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+  res.send("My first ever api!");
+});
 
+/*
 router.post("/createUser", UserController.createUser  )
-
 router.get("/getUsersData", UserController.getUsersData)
-
 router.post("/createBook", BookController.createBook  )
-
 router.get("/getBooksData", BookController.getBooksData)
-
 router.post("/updateBooks", BookController.updateBooks)
 router.post("/deleteBooks", BookController.deleteBooks)
+*/
 
-//MOMENT JS
+// MOMENT JS
+/*
 const moment = require('moment');
 router.get("/dateManipulations", function (req, res) {
-    
+
     // const today = moment();
     // let x= today.add(10, "days")
-
     // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
     // console.log(validOrNot)
-    
     const dateA = moment('01-01-1900', 'DD-MM-YYYY');
     const dateB = moment('01-01-2000', 'DD-MM-YYYY');
-
     let x= dateB.diff(dateA, "days")
     console.log(x)
-
     res.send({ msg: "all good"})
 })
-
+*/
+//1
+router.post("/createBook", authorController.createBook);
+router.get("/getBook", authorController.getBook);
+//2
+router.post("/createAuthor", authorController.createAuthor);
+router.get("/getAuthor", authorController.getAuthor);
+//3
+router.get("/getBookName", authorController.getBookName);
+//4
+router.get("/getPrice", authorController.getPrice);
 module.exports = router;
