ec5aa678a4131dee5898ed0354e49620e063be7a
Mohd Arshad <mohdarshad8668@gmail.com>
2022-12-20 20:39:21 +0530

    Assignment of author/books

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authorController = require("../controllers/controller");
-
-router.get("/test-me", function (req, res) {
-  res.send("My first ever api!");
-});
+const authorController = require("../controllers/controller.js");
 
 /*
 router.post("/createUser", UserController.createUser  )
