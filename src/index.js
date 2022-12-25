c4e8f2687d0107a241e504aea5b55e70041922e4
Mohd Arshad <mohdarshad8668@gmail.com>
2022-12-25 10:31:38 +0530

    global mw

--- src/index.js
+++ src/index.js
@@ -14,6 +14,22 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
+const assignmentMW= function (req, res, next) {
+  var currentdate = new Date(); 
+  var datetime =  currentdate.getDate() + " "
+                  + (currentdate.getMonth()+1)  + " " 
+                  + currentdate.getFullYear() + "  "  
+                  + currentdate.getHours() + ":"  
+                  + currentdate.getMinutes() + ":" 
+                  + currentdate.getSeconds();
+
+  let ip= req.ip
+  let url= req.originalUrl
+  console.log(`${datetime}  ${ip}  ${url}`)
+  next()    
+}
+
+app.use( assignmentMW )
 
 
 app.use('/', route);
